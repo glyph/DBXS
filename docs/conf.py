ca2901887b692939ee05db66eefceeaebe910dc4
Glyph <code@glyph.im>
2025-08-22 23:33:59 -0700

    furo theme
    This commit was sponsored by Devin Prater, Geoffrey Thomas, Matt
    Campbell, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = []
 
 linkcheck_ignore = [
