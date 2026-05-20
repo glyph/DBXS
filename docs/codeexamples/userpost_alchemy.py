f64055eb2c800e5a771c7ec4112335c55d1d5dbd
Glyph <code@glyph.im>
2026-05-19 21:34:35 -0700

    a version of the userpost example with SQLAlchemy queries instead of raw SQL
    This commit was sponsored by Hynek, Anika Huhn, Andy Fundinger, and my
    other patrons.  If you want to join them, you can support my work at
    https://glyph.im/patrons/.

---
+++ docs/codeexamples/userpost_alchemy.py
@@ -0,0 +1,216 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime
+from typing import AsyncIterable, Protocol
+
+from sqlalchemy import (
+    TIMESTAMP,
+    Column,
+    ForeignKeyConstraint,
+    Integer,
+    MetaData,
+    Table,
+    Text,
+    bindparam,
+    select,
+)
+
+from dbxs import many, one, query, repository, statement
+from dbxs.adapters.dbapi_twisted import adaptSynchronousDriver
+from dbxs.async_dbapi import transaction
+
+
+POSTGRES = False
+
+IntegrityError: type[Exception]
+if POSTGRES:
+    from userpost_gres import (
+        IntegrityError,
+        driverConnect,
+        driverParamStyle,
+        schemaPath,
+    )
+else:
+    # start sqlite imports
+    from userpost_sqlite import (
+        IntegrityError,
+        driverConnect,
+        driverParamStyle,
+        schemaPath,
+    )
+
+    # end sqlite imports
+
+
+with schemaPath.open() as f:
+    schema = f.read()
+
+
+# start driver
+asyncDriver = adaptSynchronousDriver(driverConnect, driverParamStyle)
+# end driver
+
+
+# start user attributes
+@dataclass
+class User:
+    postDB: PostDB
+    id: int
+    name: str
+    # end user attributes
+
+    # start user methods
+    async def post(self, text: str) -> None:
+        return await self.postDB.makePostByUser(datetime.now(), text, self.id)
+
+    def posts(self) -> AsyncIterable[Post]:
+        return self.postDB.postsForUser(self.id)
+        # end user methods
+
+
+# start post
+@dataclass
+class Post:
+    postDB: PostDB
+    postID: int
+    authorID: int
+    created: datetime
+    content: str
+    # end post
+
+
+def idcol() -> Column[int]:
+    return Column("id", Integer, primary_key=True, autoincrement=True)
+
+
+metadata = MetaData()
+userTable = Table(
+    "user",
+    metadata,
+    idcol(),
+    Column("name", Text, nullable=False),
+)
+postTable = Table(
+    "post",
+    metadata,
+    idcol(),
+    Column("created", TIMESTAMP, nullable=False),
+    Column("content", Text, nullable=False),
+    Column("author", Integer, nullable=False),
+    ForeignKeyConstraint(["author"], [userTable.c.id], ondelete="cascade"),
+)
+
+
+# start postdb protocol
+class PostDB(Protocol):
+    # start postdb methods
+    # start createUser
+    @query(
+        sql=(
+            userTable.insert()
+            .values({userTable.c.name: bindparam("name")})
+            .returning(userTable.c.id, userTable.c.id)
+        ),
+        load=one(User),
+    )
+    async def createUser(self, name: str) -> User:
+        ...
+        # end createUser
+
+    @query(
+        sql=select(userTable.c.id, userTable.c.name).where(
+            userTable.c.name == bindparam("name")
+        ),
+        load=one(User),
+    )
+    async def loadUserNamed(self, name: str) -> User:
+        ...
+
+    # start postsForUser
+    @query(
+        sql=select(*postTable.c["id", "author", "created", "content"]).where(
+            postTable.c.author == bindparam("userID")
+        ),
+        load=many(Post),
+    )
+    def postsForUser(self, userID: int) -> AsyncIterable[Post]:
+        ...
+        # end postsForUser
+
+    # start makePostByUser
+    @statement(
+        sql=postTable.insert().values(
+            {
+                postTable.c.created: bindparam("created"),
+                postTable.c.content: bindparam("content"),
+                postTable.c.author: bindparam("author"),
+            }
+        )
+    )
+    async def makePostByUser(
+        self, created: datetime, content: str, author: int
+    ) -> None:
+        ...
+        # end makePostByUser
+
+
+# start repo
+@dataclass
+class BlogRepo:
+    posts: PostDB
+    # end repo
+
+
+# start make repo
+blog = repository(BlogRepo)
+# end make repo
+
+
+# start ensureSchema
+async def ensureSchema() -> None:
+    async with transaction(asyncDriver) as c:
+        cur = await c.cursor()
+        for expr in schema.split(";"):
+            await cur.execute(expr)
+    # end ensureSchema
+
+
+# start makePostsBy
+async def makePostsBy(name: str) -> None:
+    try:
+        async with blog(asyncDriver) as db:
+            poster = await db.posts.createUser(name)
+            print(f"created poster: {poster.name}")
+    except IntegrityError:
+        print(f"user already exists: {name}")
+    async with blog(asyncDriver) as db:
+        poster = await db.posts.loadUserNamed(name)
+        await poster.post("a post")
+        await poster.post("another post")
+    # end makePostsBy
+
+
+# start readPostsBy
+async def readPostsBy(name: str) -> None:
+    async with blog(asyncDriver) as db:
+        poster = await db.posts.loadUserNamed(name)
+        async for post in poster.posts():
+            print(post.created, repr(post.content))
+    # end readPostsBy
+
+
+# start main
+async def main(reactor: object) -> None:
+    await ensureSchema()
+    await makePostsBy("bob")
+    await readPostsBy("bob")
+    # end main
+
+
+# start boilerplate
+if __name__ == "__main__":
+    from twisted.internet.task import react
+
+    react(main)
+# end boilerplate
